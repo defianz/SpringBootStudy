3e266ad9ee3d691b13486f27e74b62b52e65ecd5
Defian <iautm91@gmail.com>
2021-07-16 18:33:09 +0900

    SpringBoot JDBC Study

---
+++ springbootjdbc/src/main/java/me/defian/springbootjdbc/MySQLRunner.java
@@ -0,0 +1,43 @@
+package me.defian.springbootjdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class MySQLRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        try(Connection connection = dataSource.getConnection();) {
+
+//            connection.setAutoCommit(false);
+            System.out.println("connection.getMetaData().getURL() = " + connection.getMetaData().getURL());
+            System.out.println("connection.getMetaData().getUserName() = " + connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+            statement.executeUpdate(sql);
+//            connection.setAutoCommit(true);
+
+            // 에러 상황 롤백
+
+//            connection.close();
+        }
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1,'defian')");
+
+    }
+}
