b59d407a96684b4d4ed4b929a16dcec42ffe6846
Defian <iautm91@gmail.com>
2021-07-19 16:02:41 +0900

    SpringBootSecurity Start

---
+++ springbootsecurity2/src/main/java/me/defian/springbootsecurity2/AccountRunner.java
@@ -0,0 +1,22 @@
+package me.defian.springbootsecurity2;
+
+import me.defian.springbootsecurity2.account.Account;
+import me.defian.springbootsecurity2.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountRunner implements ApplicationRunner {
+
+    @Autowired
+    AccountService accountService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account defian  = accountService.createAccount("defian","1234");
+        System.out.println("defian.getUsername() = " + defian.getUsername());
+        System.out.println("defian.getPassword() = " + defian.getPassword());
+    }
+}
---
+++ springbootsecurity2/src/test/java/me/defian/springbootsecurity2/Springbootsecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.defian.springbootsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootsecurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
